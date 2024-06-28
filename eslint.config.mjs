704bf8881e89ea68bb967271bc27633a6b673d01
tanvirch0750 <tanvirch7575@gmail.com>
2024-06-28 17:12:28 +0600

    project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+    {
+        ignores: ['node_modules/', 'dist/'], // Add your ignore patterns here
+    },
+    {
+        files: ['/*.ts'], // Specify file extensions to lint
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'error',
+            // to enforce using type for object type definitions, can be type or interface
+            '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+            '@typescript-eslint/no-unused-expressions': 'error',
+            '@typescript-eslint/prefer-const': 'error',
+            '@typescript-eslint/no-console': 'warn',
+            '@typescript-eslint/no-undef': 'error',
+        },
+        globals: {
+            process: 'readonly',
+        },
+        extends: [
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'prettier',
+        ],
+    },
+
+    { files: ['/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+];
