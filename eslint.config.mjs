93a70274bd9c3f6f03f88481ceb7934393d3b981
tanvirch0750 <tanvirch7575@gmail.com>
2024-07-08 13:30:57 +0600

    Change eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,67 @@
+import eslint from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
     {
         ignores: ['node_modules/', 'dist/'], // Add your ignore patterns here
     },
     {
-        files: ['/*.ts'], // Specify file extensions to lint
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-        },
-
         rules: {
-            '@typescript-eslint/no-unused-vars': ['error'],
-            // to enforce using type for object type definitions, can be type or interface
-            '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-            '@typescript-eslint/no-unused-expressions': 'error',
-            '@typescript-eslint/prefer-const': 'error',
-            '@typescript-eslint/no-console': 'error',
-            '@typescript-eslint/no-undef': 'error',
+            'no-unused-vars': 'error',
+            'no-undef': 'error',
+            'no-console': 'warn',
+            'prefer-const': 'error',
+            'no-unused-expressions': 'error',
         },
-        globals: {
-            process: 'readonly',
-        },
-        extends: [
-            'eslint:recommended',
-            'plugin:@typescript-eslint/recommended',
-            'prettier',
-        ],
     },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
+);
 
-    { files: ['/*.{js,mjs,cjs,ts}'] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-];
+// import globals from 'globals';
+// import pluginJs from '@eslint/js';
+// import tseslint from 'typescript-eslint';
+
+// export default [
+//     {
+//         ignores: ['node_modules/', 'dist/'], // Add your ignore patterns here
+//     },
+//     {
+//         files: ['/*.ts'], // Specify file extensions to lint
+//         languageOptions: {
+//             ecmaVersion: 'latest',
+//             sourceType: 'module',
+//         },
+
+//         rules: {
+//             '@typescript-eslint/no-unused-vars': ['error'],
+//             // to enforce using type for object type definitions, can be type or interface
+//             '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+//             '@typescript-eslint/no-unused-expressions': 'error',
+//             '@typescript-eslint/prefer-const': 'error',
+//             '@typescript-eslint/no-console': 'error',
+//             '@typescript-eslint/no-undef': 'error',
+//         },
+//         globals: {
+//             process: 'readonly',
+//         },
+//         extends: [
+//             'eslint:recommended',
+//             'plugin:@typescript-eslint/recommended',
+//             'prettier',
+//         ],
+//     },
+
+//     { files: ['/*.{js,mjs,cjs,ts}'] },
+//     { languageOptions: { globals: globals.browser } },
+//     pluginJs.configs.recommended,
+//     ...tseslint.configs.recommended,
+// ];
